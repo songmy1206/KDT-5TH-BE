d67ecacc1dde1105683dedaf6b3550b4f8e6432b
songminyoung <songmy1206@gmail.com>
2023-03-10 15:50:01 +0900

    230310 be

--- 03_express/routes/posts.js
+++ 03_express/routes/posts.js
@@ -9,26 +9,13 @@ const POST = [
   },
 ];
 
-// http://localhost:4000/users
+// http://localhost:4000/posts
 router.get('/', (req, res) => {
-  res.render('posts', { POST, userCounts: POST.length });
+  res.render('posts', { POST, postCounts: POST.length });
 });
 
 router.post('/add', (req, res) => {
-  if (Object.keys(req.query).length >= 1) {
-    if (req.query.title && req.query.content) {
-      const newPost = {
-        title: req.query.title,
-        content: req.query.content,
-      };
-      POST.push(newPost);
-      res.send('글 등록완료');
-    } else {
-      const err = new Error('입력 오류');
-      err.statusCode = 400;
-      throw err;
-    }
-  } else if (req.body) {
+  if (Object.keys(req.body).length >= 1) {
     if (req.body.title && req.body.content) {
       const newPost = {
         title: req.body.title,
@@ -37,7 +24,7 @@ router.post('/add', (req, res) => {
       POST.push(newPost);
       res.redirect('/posts');
     } else {
-      const err = new Error('폼태그 입력확인바람');
+      const err = new Error('폼데이터 입력확인바람');
       err.statusCode = 400;
       throw err;
     }
